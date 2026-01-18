a6e57af0bd91e1f13373c538d939dacc835af612
Majata Hichimi <hichimimajata@gmail.com>
2026-01-18 23:30:19 +0200

    closing lesson on functions

---
+++ functions.js
@@ -0,0 +1,21 @@
+/**
+ * functions are a block of reuseable code that can be called to perform a specific task
+ * functions can only run once they are called 
+ */
+
+//function declaration
+function greetUser(){
+    console.log("Hello User");
+
+}
+
+//function call
+//when a  function is run it is called a functio invokation
+greetUser();
+//can be displayed in the standard output
+console.log(greetUser());
+
+
+
+
+
