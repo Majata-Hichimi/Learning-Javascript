ae90bb5ecd2a345abfcaecca9b5ea2ae75f94024
Majata Hichimi <hichimimajata@gmail.com>
2026-01-19 17:24:35 +0200

    Basic DOM in Javascript

---
+++ DOM/DOM.js
@@ -0,0 +1,23 @@
+/**
+ * DOM(Document Object Model)
+ * Is used to access html elements 
+ * the most common accessmethod is access by id
+ */
+
+/**
+ * DOM API is a set of methods and properties that allow Javascript to alter HTML content
+ * An API Method is an action you can perform on HTML
+ * An API property is a value that you can access on an HTML element
+ */
+
+//the word(document) refers to the html document itself
+//the function(.getElementById()) refers to the method
+//the word(innerHTML) reters to a property
+
+/**
+ * A DOM API provides the ablity to
+ * find and select elements
+ * alter webpage content
+ * listen for user input
+ * Javascript uses the browser to access the DOM through the API
+ */
