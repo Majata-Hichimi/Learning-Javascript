0517a83655551007b3aeba64e73c015cd89101ad
Majata Hichimi <hichimimajata@gmail.com>
2026-01-22 22:22:06 +0200

    Learning Javascript Control flow

---
+++ async/asych.js
@@ -0,0 +1,18 @@
+/**
+ * control flow
+ * this refers to the order in which code flows
+ * by default Javascript executes code sequencially
+ * Javascript runs on a single thread(one thing at a time)
+ * -Asychronous flow(refers to the handling of tasks that take an unknown amount of time to complete)
+ * -Asychronous code allows for tasks to be executed in the background 
+ * 
+ */
+
+/**
+ * Asychronous Patterns:
+ * -Events(actions that occur in the browser due to user interaction)
+ * -Callbacks(this is a function passed as a parameter to another function
+ *      intended to be executed later due to an event)
+ * -Promises(this represent a vaue that may be avalable at anytime or not at all)
+ * -Async/Await()
+ */
